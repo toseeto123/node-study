ede37277d795f6728a7da4375fe2605942354654
toseeto123 <taipoone@gmail.com>
2023-05-19 20:55:03 +0900

    env 구현

--- day5/app.js
+++ day5/app.js
@@ -1,24 +1,49 @@
 const express = require('express');
 const path = require('path');
-
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 
 app.set('port',process.env.PORT || 3000);
+//미들웨어 순서는 중요하다.
+app.use(morgan('combined')); //개발시 dev, 배포시 combined 사용
 
-app.use((req,res, next ) =>{ //(req부터 ~} 까지가 미들웨어이다.)
-  console.log('1 모든요청에 실행'); 
-  next();
-},(req,res,next)=>{
-    try{
-        console.log(asdad);
-    }catch(error){
-        next(error); //넥스트에 인수가 들어가 있다면 에러처리 미들웨어로 들어간다.
+//app.use('요청경로',express.static('실제경로')); //정적파일(html,css) 보낼때 사용
+app.use('/', (req,res,next)=>{
+    if (req.session.id){ // 로그인한 경우 해당 파일 제공
+        express.static(__dirname, '')(req, res, next) //보안을 유지하는데 좋다. 파일을 해당폴더에서 찾을경우 next를 진행하지않음
+    }else{ // 아닌경우 파일 제공하지않음
+        next();
     }
+   
 });
+app.use(cookieParser('password'));
+//개인의 저장공간을 만들어주는 express-session
+app.use(session({
+    resave:false,
+    saveUninitialized:false,
+    secret:'password',
+    cookie:{
+        httpOnly: true,
+    },
+    name: 'connect.sid', // 서명되어있기때문에 읽을수없는 문자열
 
-app.get('/', (req,res)=>{  
- res.sendFile(path.join(__dirname,'./index.html')); //알아서 fs모듈을 사용하여 파일을 읽어서 불러온다
+}))
+app.use(express.json()); // client 에서 json 보낼때 파싱하여사용
+//client에서 form태그 보낼때 파싱하여 사용 true면 qs,false면 querystring 사용
+app.use(express.urlencoded({ extended: true }));  
+//form에서 이미지나 파일 보낼때는 urlencoded가 처리를 못해서 다른 종류를 사용함(multer)
+
+app.use((req,res,next)=>{
+    req.session.data = 'namsu비번';
+    //req.data >> 요청한번에만 data를 전송할때 사용
+})
+
+app.get('/', (req,res,next)=>{  
+    req.session.data // namsu비번 >> 다만 세션이기때문에 영구적으로 남음
+     res.sendFile(path.join(__dirname,'./index.html')); //알아서 fs모듈을 사용하여 파일을 읽어서 불러온다
  next('route');
 },(req,res)=>{
     console.log('실행되지않습니다.'); //next의 경우 다음 라우터로 실행된다.
@@ -42,12 +67,11 @@ app.get('/about', (req,res)=>{
 });
 
 app.use((req,res,next)=>{ //404처리 미들웨어 ()
-    res.send('404입니다.'); //status(404)가 생략된 상태
+    res.status(404).send('404입니다.'); //status(404)가 생략된 상태
 })
 
 app.use((err,req,res,next)=>{ //err 미들웨어는 반드시 4개를 다 사용해줘야한다.
     console.error(err);
-    
     res.status(500).send('에러났지만 원인은 모릅니다.');
 })
 
